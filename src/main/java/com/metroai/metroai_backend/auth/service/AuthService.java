f2af66345489240e72595255b1d3b21cbcad864f
Akash Sharma <akashkumarsharmaalp@gmail.com>
2026-06-13 19:59:03 +0530

    feat(auth): add BCrypt password hashing

--- src/main/java/com/metroai/metroai_backend/auth/service/AuthService.java
+++ src/main/java/com/metroai/metroai_backend/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package com.metroai.metroai_backend.auth.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.metroai.metroai_backend.auth.dto.RegisterRequest;
@@ -13,10 +14,12 @@ import com.metroai.metroai_backend.auth.repository.UserRepository;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public AuthService(UserRepository userRepository,PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+}
 
     public String register(RegisterRequest request) {
 
@@ -28,7 +31,7 @@ public class AuthService {
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
-                .password(request.password()) // plain text for now
+                .password(passwordEncoder.encode(request.password()))
                 .role(Role.USER)
                 .createdAt(LocalDateTime.now())
                 .build();
